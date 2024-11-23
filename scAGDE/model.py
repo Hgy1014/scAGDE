387d37c8829c4a13881c7f0b55aee5b7cd491209
Hgy1014 <1248959179@qq.com>
2024-11-23 11:57:10 +0000

    AnnData support

--- scAGDE/model.py
+++ scAGDE/model.py
@@ -28,7 +28,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        # self.val_loss_min = np.Inf
         self.delta = delta
 
     def __call__(self, val_loss, model):
@@ -45,16 +45,16 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            # self.save_checkpoint(val_loss, model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        path = os.path.join(self.save_path, 'best_network.pth')
-        torch.save(model.state_dict(), path)
-        self.val_loss_min = val_loss
+    # def save_checkpoint(self, val_loss, model):
+    #     '''Saves model when validation loss decrease.'''
+    #     if self.verbose:
+    #         print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+    #     path = os.path.join(self.save_path, 'best_network.pth')
+    #     torch.save(model.state_dict(), path)
+    #     self.val_loss_min = val_loss
 
 class BaseModel(nn.Module):
     def __init__(self):
